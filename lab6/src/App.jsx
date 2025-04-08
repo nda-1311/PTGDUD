f0d542b1d098484a99fb8cf104b9bbc7463246f9
nda-1311 <1dap2xoe@gmail.com>
2025-04-08 17:07:48 +0700

    Hoan thien yc5 isActive + yc6 react router

--- lab6/src/App.jsx
+++ lab6/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './components/Layout'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Layout/>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/*" element={<Layout />} />
+    </Routes>
+  </Router>
   )
 }
 
